a20675336d5b28ab3a36235dee648126364ccb54
Narcob47 <137599138+Narcob47@users.noreply.github.com>
2025-01-05 13:21:14 +0200

    Update Topics.tsx

--- app/components/Home/Topics.tsx
+++ app/components/Home/Topics.tsx
@@ -4,7 +4,7 @@ const Topics = () => {
   return (
     <section className="py-16 text-center">
       <h2 style={{ color: COLORS.black }} className="text-3xl font-semibold">What’s Our Books Are About</h2>
-      <p style={{ color: COLORS.black }} className=" mt-4 max-w-xl mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p style={{ color: COLORS.black }} className=" mt-4 max-w-xl mx-auto"></p>
       <div className="mt-8 grid grid-cols-2 gap-4 max-w-xl mx-auto">
         {['Holistic Ministry', 'Service', 'Health and Wellness', 'Education', 'Family Values', 'Diligent Study', 'SOP', 'Pioneers' ].map((topic, i) => (
           <div key={i} className="flex items-center space-x-2">
