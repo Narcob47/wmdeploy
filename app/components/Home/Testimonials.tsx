898c71ada5b2ab66eda9a2e69a5523aab9ffee72
Narcob47 <137599138+Narcob47@users.noreply.github.com>
2025-01-05 18:01:59 +0200

    Update Testimonials.tsx

--- app/components/Home/Testimonials.tsx
+++ app/components/Home/Testimonials.tsx
@@ -3,7 +3,7 @@ import { COLORS } from '../../constants/colors';
 const Testimonials = () => {
   return (
     <section className="py-16 text-center bg-gray-50">
-      <h2 style={{ color: COLORS.black }} className="text-3xl font-semibold">What People Say about the Redex Book</h2>
+      <h2 style={{ color: COLORS.black }} className="text-3xl font-semibold">What People Say about the WatchMission Society</h2>
       <p style={{ color: COLORS.black }} className="text-gray-500 mt-4 max-w-xl mx-auto"></p>
       <div className="mt-8">
         <img src="https://img.freepik.com/premium-psd/3d-customer-feedback-review-testimonial-design-social-media-instagram-post-template-with-mockup_125322-328.jpg" alt="Testimonials Illustration" className="mx-auto w-[20rem]" />
