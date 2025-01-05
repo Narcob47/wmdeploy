5b41ceca7fc802fd302133836b96cb1802c17d45
Narcob47 <137599138+Narcob47@users.noreply.github.com>
2025-01-05 13:30:10 +0200

    Update Contactus.tsx

--- app/components/Home/Contactus.tsx
+++ app/components/Home/Contactus.tsx
@@ -3,7 +3,8 @@ import { COLORS } from '../../constants/colors';
 const ContactUs = () => {
   return (
     <section className="py-16 text-center bg-red-100 rounded-lg mx-4">
-      <h2 className="text-xl font-semibold mb-4 text-black">If you wish to support our work through a donation. <br> Please send it to +260 978 9972 67 or email us</h2>
+      <h2 className="text-xl font-semibold mb-4 text-black">If you wish to support our work through a donation. </h2>
+      <h2 className="text-xl font-semibold mb-4 text-black">Please send it to +260 978 9972 67 or email us</h2>
       <button style={{ borderColor: COLORS.primary, color: COLORS.primary }} className="border-2 text-white px-6 py-2 rounded-md">Join us</button>
     </section>
   );
